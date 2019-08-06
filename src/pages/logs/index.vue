84b3ee91bff7f28785fad22a8c749a3df3bd4f5c
gengzeng <1041355591@qq.com>
2019-08-06 16:57:37 +0800

    需要拉

---
+++ src/pages/logs/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="homeWrap"></div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  methods: {},
+
+  created() {
+    // let app = getApp()
+  }
+};
+</script>
+
+<style scoped>
+.homeWrap {
+  height: 100%;
+  width: 100%;
+  background: red;
+}
+</style>
