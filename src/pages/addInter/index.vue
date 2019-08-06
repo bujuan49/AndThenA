d62b1b4e8b7a269e7bad7d49aef2871602b96b6d
gengzeng <1041355591@qq.com>
2019-08-06 21:45:49 +0800

    基本_4

---
+++ src/pages/addInter/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="addInterWrap">12312123</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  //传入组件
+  components: {},
+
+  methods: {},
+
+  created() {
+    // let app = getApp()
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.indexWrap {
+  width: 100%;
+  height: 100%;
+}
+</style>
