58b5eb22fbe70f1261c53b96f89b85ebfcafba42
gengzeng <1041355591@qq.com>
2019-08-06 19:45:09 +0800

    ‘面试列表—_1

--- src/pages/interviewList/index.vue
+++ src/pages/interviewList/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="interviewListWrap"></div>
+  <div class="interviewListWrap">
+    <ul class="interviewListHeader">
+      <li class="active">未开始</li>
+      <li>已打卡</li>
+      <li>以放弃</li>
+      <li>全部</li>
+    </ul>
+    <div class="interviewMain">
+      <div class="interviewMainBox"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,8 +29,35 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.indexWrap {
+.interviewListWrap {
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  background: rgb(238, 238, 238);
+}
+.interviewListHeader {
+  background: #fff;
+  border-top: 1px solid #ccc;
+  display: flex;
+  justify-content: space-around;
+  li {
+    text-align: center;
+    padding: 15rpx 0;
+  }
+}
+.interviewMain {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  .interviewMainBox {
+    height:260rpx;
+    background: #fff;
+    margin-top: 16rpx;
+  }
+}
+.active {
+  color: rgb(39, 133, 195);
+  border-bottom: 1px solid rgb(39, 133, 195);
 }
 </style>
