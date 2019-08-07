583206f6f7fb197b0dbdbf050bb2416a08c3bf9f
Yufeng <17521064533@163.com>
2019-08-07 11:55:50 +0800

    dev

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import Vue from 'vue'
-import App from './App'
-
-Vue.config.productionTip = false
-App.mpType = 'app'
-
-const app = new Vue(App)
-app.$mount()
-=======
 import Vue from 'vue'
 import App from './App'
 import store from "./store"
@@ -16,4 +6,3 @@ App.mpType = 'app'
 Vue.prototype.$store = store;
 const app = new Vue(App)
 app.$mount()
->>>>>>> whp
